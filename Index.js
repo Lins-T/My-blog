aa1569017d57d917493ddd54aa7ae982fef4a84e
Lins-T <110426000+Lins-T@users.noreply.github.com>
2022-08-01 21:10:47 +0000

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,31 @@
+function Join() {
+			var currentMenu = document.getElementById("Joinlink");
+			
+			Joinlink.style.bottom = "-100%"
+			
+			if(currentMenu == "-100%") {
+			
+							Joinlink.style.bottom = "0";
+			}
+			
+			
+			else {
+				Joinlink.style.bottom = "0";			
+			}
+}
+
+
+
+function quit() {
+				var currentMenu = document.getElementById("Joinlink")	;
+			
+			Joinlink.style.bottom = "0";
+			
+			if(currentMenu = "0") {
+							Joinlink.style.bottom = "-100%";
+			}
+			else {
+				Joinlink.style.bottom = "0";			
+			}
+}
+
